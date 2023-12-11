b2c39bc628f4f9e06572357e21a25fd6917050c9
fizzydrinks <gabriel@lins.dev>
2023-12-11 17:36:35 -0300

    Add skip function

--- src/components/GameOfLife.tsx
+++ src/components/GameOfLife.tsx
@@ -40,18 +40,16 @@ const GameOfLife = () => {
     [x - 1, y - 1],
   ];
 
-  const neighbourCount = (x: number, y: number): number => {
-    const all = neighbourCoords(x, y).map(
-      ([x, y]) => liveCells[x]?.[y] === true,
-    );
+  const neighbourCount = (state: GameState, x: number, y: number): number => {
+    const all = neighbourCoords(x, y).map(([x, y]) => state[x]?.[y] === true);
 
     return all.reduce((a, i) => a + Number(i), 0);
   };
 
-  const next = () => {
+  const getNextState = (state: GameState): GameState => {
     const newState: GameState = {};
 
-    Object.entries(liveCells).forEach(([x, row]) => {
+    Object.entries(state).forEach(([x, row]) => {
       Object.entries(row)
         .filter(([, active]) => active)
         .forEach(([y]) => {
@@ -59,11 +57,11 @@ const GameOfLife = () => {
           const ny = Number(y);
 
           const deadNeighbours = neighbourCoords(nx, ny).filter(
-            ([x, y]) => liveCells[x]?.[y] !== true,
+            ([x, y]) => state[x]?.[y] !== true,
           );
 
           deadNeighbours.forEach(([x, y]) => {
-            const liveNeighbours = neighbourCount(x, y);
+            const liveNeighbours = neighbourCount(state, x, y);
             if (liveNeighbours !== 3) return;
 
             if (!newState[x]) newState[x] = {};
@@ -71,7 +69,7 @@ const GameOfLife = () => {
             newState[x][y] = true;
           });
 
-          const liveNeighbours = neighbourCount(nx, ny);
+          const liveNeighbours = neighbourCount(state, nx, ny);
           if (liveNeighbours < 2 || liveNeighbours > 3) return;
 
           if (!newState[nx]) newState[nx] = {};
@@ -80,16 +78,28 @@ const GameOfLife = () => {
         });
     });
 
+    return newState;
+  };
+
+  const next = (steps = 1) => {
+    let newState = liveCells;
+    for (let i = 0; i < steps; i++) {
+      newState = getNextState(newState);
+    }
+
     setLiveCells(newState);
   };
 
+  const [speed, setSpeed] = useState(1);
   const [playing, setPlaying] = useState(false);
   const togglePlay = () => {
     if (!playing) next();
     setPlaying((p) => !p);
   };
 
-  useInterval(next, playing ? 1000 : null);
+  useInterval(next, playing ? 1000 / speed : null);
+
+  const [skip, setSkip] = useState<number>(5);
 
   return (
     <>
@@ -100,22 +110,62 @@ const GameOfLife = () => {
         onNavigate={moveCenter}
         center={center}
       />
-      <div className='bg-white p-4 text-lg text-black absolute'>
-        <button onClick={next} className='rounded border-2 border-black p-2'>
-          Next
-        </button>
-        <button
-          onClick={togglePlay}
-          className='rounded border-2 border-black p-2'
+      <div className='absolute bottom-0 w-full flex justify-center'>
+        <div
+          className='
+            m-2 mx-auto flex justify-center gap-2 items-center
+            bg-gray-700 py-2 px-6 text-lg rounded
+          '
         >
-          {playing ? 'Stop' : 'Play'}
-        </button>
-        <button
-          onClick={() => setLiveCells({})}
-          className='rounded border-2 border-black p-2'
-        >
-          Clear
-        </button>
+          <button
+            onClick={() => next()}
+            className='rounded border-2 border-gray-500 p-2'
+          >
+            Next
+          </button>
+          <button
+            onClick={togglePlay}
+            className='rounded border-2 border-gray-500 p-2'
+          >
+            {playing ? 'Stop' : 'Play'}
+          </button>
+          <button
+            onClick={() => setLiveCells({})}
+            className='rounded border-2 border-gray-500 p-2'
+          >
+            Clear
+          </button>
+          <div className='p-2'>
+            <label htmlFor='speed' className='block text-center'>
+              Speed
+            </label>
+            <input
+              id='speed'
+              type='range'
+              min={1}
+              max={5}
+              onChange={(e) => setSpeed(Number(e.target.value))}
+            />
+          </div>
+          <div className='p-2'>
+            <label className='block text-center'>
+              Advance{' '}
+              <input
+                type='number'
+                onChange={(e) => setSkip(Number(e.target.value))}
+                value={skip}
+                className='text-black w-12 text-right rounded bg-gray-500 my-1'
+              />{' '}
+              steps
+            </label>
+            <button
+              className='w-full border-2 rounded border-gray-500 p-1'
+              onClick={() => next(skip)}
+            >
+              Go
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
