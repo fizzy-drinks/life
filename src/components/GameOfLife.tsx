7ac677b70baa862dc1768c1cab4221e7ec587053
fizzydrinks <gabriel@lins.dev>
2023-12-11 16:49:07 -0300

    Create next step function

--- src/components/GameOfLife.tsx
+++ src/components/GameOfLife.tsx
@@ -28,6 +28,60 @@ const GameOfLife = () => {
     setCenter((prev) => [prev[0] - x, prev[1] - y]);
   };
 
+  const neighbourCoords = (x: number, y: number): [number, number][] => [
+    [x, y - 1],
+    [x + 1, y - 1],
+    [x + 1, y],
+    [x + 1, y + 1],
+    [x, y + 1],
+    [x - 1, y + 1],
+    [x - 1, y],
+    [x - 1, y - 1],
+  ];
+
+  const neighbourCount = (x: number, y: number): number => {
+    const all = neighbourCoords(x, y).map(
+      ([x, y]) => liveCells[x]?.[y] === true,
+    );
+
+    return all.reduce((a, i) => a + Number(i), 0);
+  };
+
+  const next = () => {
+    const newState: GameState = {};
+
+    Object.entries(liveCells).forEach(([x, row]) => {
+      Object.entries(row)
+        .filter(([, active]) => active)
+        .forEach(([y]) => {
+          const nx = Number(x);
+          const ny = Number(y);
+
+          const deadNeighbours = neighbourCoords(nx, ny).filter(
+            ([x, y]) => liveCells[x]?.[y] !== true,
+          );
+
+          deadNeighbours.forEach(([x, y]) => {
+            const liveNeighbours = neighbourCount(x, y);
+            if (liveNeighbours !== 3) return;
+
+            if (!newState[x]) newState[x] = {};
+
+            newState[x][y] = true;
+          });
+
+          const liveNeighbours = neighbourCount(nx, ny);
+          if (liveNeighbours < 2 || liveNeighbours > 3) return;
+
+          if (!newState[nx]) newState[nx] = {};
+
+          newState[nx][ny] = true;
+        });
+    });
+
+    setLiveCells(newState);
+  };
+
   return (
     <>
       <Grid center={center} />
@@ -37,6 +91,12 @@ const GameOfLife = () => {
         onNavigate={moveCenter}
         center={center}
       />
+      <button
+        onClick={next}
+        className='bg-white p-4 text-lg text-black absolute'
+      >
+        Next
+      </button>
     </>
   );
 };
